f58ea16698d356c54807aea7e36b10024dd2629f
Nikhil <nikhil23.rao@gmail.com>
2021-03-06 11:09:04 -0800

    Initial Commit.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import Downshift from "downshift";
+import "./App.css";
+import { myBooks } from "./bookData";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Downshift
+      onChange={(bookSelected) =>
+        alert(
+          bookSelected
+            ? `You Selected ${bookSelected.title} here is some info. Authors: ${bookSelected.authors}`
+            : "No Results"
+        )
+      }
+      itemToString={(item) => (item ? item.title : "")}
+    >
+      {({
+        getInputProps,
+        getItemProps,
+        getLabelProps,
+        getMenuProps,
+        isOpen,
+        inputValue,
+        highlightedIndex,
+        selectedItem,
+        getRootProps,
+      }) => (
+        <React.Fragment>
+          <div>
+            <label {...getLabelProps()}>Search A Book: </label>
+            <div style={{ display: "inline-block" }} {...getRootProps()}>
+              <input {...getInputProps()} />
+            </div>
+            <ul {...getMenuProps()}>
+              {isOpen
+                ? myBooks
+                    .filter(
+                      (book) =>
+                        !inputValue ||
+                        book.title
+                          .toString()
+                          .match(new RegExp(".*" + inputValue + ".*", "i"))
+                    )
+                    // .slice(0, 10)
+                    .map((item, index) => (
+                      <li
+                        {...getItemProps({
+                          key: item.ratings_count,
+                          index,
+                          item,
+                          style: {
+                            backgroundColor:
+                              highlightedIndex === index ? "gray" : "white",
+                            fontWeight:
+                              selectedItem === item ? "bold" : "normal",
+                            width: "50%",
+                            cursor: "pointer",
+                          },
+                        })}
+                      >
+                        {item.title}
+                      </li>
+                    ))
+                : null}
+            </ul>
+          </div>
+        </React.Fragment>
+      )}
+    </Downshift>
   );
 }
 
